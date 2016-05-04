bae336f9c6b189ec5af7bd9794d1737fc69b5792
prio101 <miprio101@gmail.com>
2016-05-04 18:11:23 +0600

    Jobs Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  resources :jobs
+  devise_for :admins , :jobs
   resources :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'jobs#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
